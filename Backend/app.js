3d391c9318e0e59b5484b50c8b620115b4d547b3
Himanshu Singh <147912104+himanshubabber@users.noreply.github.com>
2026-09-25 18:16:52 +0530

    Add new CORS origin for production environment

--- Backend/app.js
+++ Backend/app.js
@@ -28,6 +28,7 @@ const allowedOrigins = [
   "http://localhost:5173",
   "http://localhost:5174",
   "http://localhost:3000",
+  https://karigar-9rb90t6ty-himanshubabbers-projects.vercel.app
 ].filter(Boolean);
 
 app.use(cors({
